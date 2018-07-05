3f5a35805bfd68c8feca00041ec277c1aaa5ed60
Arthurzhangsheng <33758384+Arthurzhangsheng@users.noreply.github.com>
2018-07-05 11:47:11 +0800

    Add files via upload

--- rename.py
+++ rename.py
@@ -1,17 +1,15 @@
+
 #coding:utf8
-import os;
+import os
 
 def rename():
         i=0
-        path=r"J:\samples\buildingskin";
-        filelist=os.listdir(path)#该文件夹下所有的文件（包括文件夹）
-        for files in filelist:#遍历所有文件
-            i=i+1
-            Olddir=os.path.join(path,files);#原来的文件路径                
-            if os.path.isdir(Olddir):#如果是文件夹则跳过
-                    continue;
-            filename=os.path.splitext(files)[0];#文件名
-            filetype=os.path.splitext(files)[1];#文件扩展名
-            Newdir=os.path.join(path,str(i)+filetype);#新的文件路径
-            os.rename(Olddir,Newdir)#重命名
+        path=r"E:\GAN\maya\day5\WGAN-WGAN-GP_with_own_image\samples\buildingskin"
+        for i in range(487):
+            d = i+1
+            olddir = path + '/' + "buildingskin ({})".format(d) + ".png"
+            newdir = path + '/' + "buildingskin({})".format(d) + ".png"
+            print(olddir)
+            print(newdir)
+            os.rename(olddir,newdir)#重命名
 rename()
