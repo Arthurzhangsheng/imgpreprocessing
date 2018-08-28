aacc61f1194962f3fd048116831e730490f57f13
Arthurzhangsheng <33758384+Arthurzhangsheng@users.noreply.github.com>
2018-08-28 19:37:26 +0800

    批量分解视频为图片（含子文件夹内视频）

---
+++ ffmpeg_img_alldir.py
@@ -0,0 +1,45 @@
+#encoding = utf-8
+import subprocess
+import os
+
+
+root_dir = r"E:\seagate2\real"
+pic_out_dir = root_dir + "/" +"frames"
+rate = 0.2 #裁剪帧率
+frame_format = "jpg" #jpg体积小,png速度快
+
+if not os.path.isdir(pic_out_dir):
+    os.makedirs(pic_out_dir)
+
+file_list = []
+
+def getfile(root_dir):
+    if os.path.isdir(root_dir):
+        for fpathe, dirs, files in os.walk(root_dir):
+            #递归遍历所有子文件夹
+            if len(dirs)>0:    
+                for dir in dirs:
+                    tem_path = os.path.join(fpathe,dir)
+                    getfile(tem_path)
+                 
+            for file in files:
+                if str(file).lower().endswith('.mp4') \
+                or str(file).lower().endswith('.avi') \
+                or str(file).lower().endswith('.mkv') \
+                or str(file).lower().endswith('.flv'):
+                    #获取文件名,不含后缀
+                    shortname, _ = os.path.splitext(file)
+                    video_path = os.path.join(root_dir, file)
+                    print(file)               
+                    subprocess.call(["ffmpeg", '-i', video_path, "-r", "0.2", "-f", "image2", pic_out_dir+'/'+shortname+'-%05d.'+frame_format])
+                    print("处理{}完成".format(file))
+                    file_list.append(file)
+
+        print("共处理了{}个视频".format(str(len(file_list))))
+
+    else:
+        print("路径不对,请确认路径的是不是写错了")
+
+
+if __name__ == "__main__":
+    getfile(root_dir)
