6224d475fb85b61ddb34dc82be6f21fa5f7aa6b9
Arthurzhangsheng <33758384+Arthurzhangsheng@users.noreply.github.com>
2018-08-31 17:31:54 +0800

    Add files via upload

--- ffmpeg_img_alldir.py
+++ ffmpeg_img_alldir.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 
 
-root_dir = r"E:\seagate2\real"
+root_dir = r"D:\seagate2\DEEPFAKES\test"
 pic_out_dir = root_dir + "/" +"frames"
 rate = 0.2 #裁剪帧率
 frame_format = "jpg" #jpg体积小,png速度快
@@ -11,29 +11,23 @@ frame_format = "jpg" #jpg体积小,png速度快
 if not os.path.isdir(pic_out_dir):
     os.makedirs(pic_out_dir)
 
+format_list = [".mp4", ".avi", ".mkv", ".flv"]
 file_list = []
 
 def getfile(root_dir):
     if os.path.isdir(root_dir):
         for fpathe, dirs, files in os.walk(root_dir):
-            #递归遍历所有子文件夹
-            if len(dirs)>0:    
-                for dir in dirs:
-                    tem_path = os.path.join(fpathe,dir)
-                    getfile(tem_path)
-                 
-            for file in files:
-                if str(file).lower().endswith('.mp4') \
-                or str(file).lower().endswith('.avi') \
-                or str(file).lower().endswith('.mkv') \
-                or str(file).lower().endswith('.flv'):
+            for _file in files:
+                #判断是否为视频格式文件
+                if os.path.splitext(_file)[-1].lower() in format_list:
                     #获取文件名,不含后缀
-                    shortname, _ = os.path.splitext(file)
-                    video_path = os.path.join(root_dir, file)
-                    print(file)               
-                    subprocess.call(["ffmpeg", '-i', video_path, "-r", "0.2", "-f", "image2", pic_out_dir+'/'+shortname+'-%05d.'+frame_format])
-                    print("处理{}完成".format(file))
-                    file_list.append(file)
+                    shortname = os.path.splitext(_file)[0]
+                    #创建当前视频的带路径文件名
+                    video_path = os.path.join(fpathe, _file)
+                    print(_file)               
+                    subprocess.call(["ffmpeg", '-i', video_path, "-r", str(rate), "-f", "image2", pic_out_dir+'/'+shortname+'-%05d.'+frame_format])
+                    print("处理{}完成".format(_file))
+                    file_list.append(_file)
 
         print("共处理了{}个视频".format(str(len(file_list))))
 
