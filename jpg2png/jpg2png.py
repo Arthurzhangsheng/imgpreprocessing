542527df910ee8bba3cd09d7a94617554312be07
Arthurzhangsheng <33758384+Arthurzhangsheng@users.noreply.github.com>
2019-06-08 13:53:00 +0800

    Add files via upload

--- jpg2png/jpg2png.py
+++ jpg2png/jpg2png.py
@@ -8,23 +8,25 @@ from tqdm import tqdm
 '''
 
 #待处理图片文件夹
-img_folder = r"F:\zs\DeepFaceLabCUDA-0219-final\workspace\AV\benzhuangling\1024aligned"
+jpg_folder = r"F:\zs\DeepFaceLabCUDA-0219-final\workspace\AV\guchuanyizhi\1024aligned"
 #处理后保存的文件夹
-result_folder =r"F:\zs\DeepFaceLabCUDA-0219-final\workspace\AV\benzhuangling\512png" 
+png_folder =r"F:\zs\DeepFaceLabCUDA-0219-final\workspace\AV\guchuanyizhi\512png" 
 #是否裁剪
 RESIZE = True
 #缩小后的尺寸
 target_size = (512,512)
-interpolation = cv2.INTER_AREA #缩小推荐用cv2.INTER_AREA
+#缩放的插值方式
+interpolation = cv2.INTER_AREA #如果要缩小图像，通常推荐使用#INTER_AREA插值效果最好，而要放大图像，通常使用INTER_CUBIC(速度较慢，但效果最好)，或者使用INTER_LINEAR(速度较快，效果还可以)。
 
-if not os.path.exists(result_folder):
-    os.makedirs(result_folder)
+#自动创建保存文件夹
+if not os.path.exists(png_folder):
+    os.makedirs(png_folder)
 
-for name in tqdm(glob.glob(f'{img_folder}/*.jpg'), ncols=10):  
+for name in tqdm(glob.glob(f'{jpg_folder}/*.jpg'), ncols=10):  
     img = cv2.imread(name)
     if RESIZE:
         img = cv2.resize(img, target_size, interpolation= interpolation)
     basename = os.path.basename(name).split(".")[0]#获取带后缀的基本文件名后，分割字符串，得到无后缀名字
     
-    cv2.imwrite(f'{result_folder}/{basename}.png',img)
+    cv2.imwrite(f'{png_folder}/{basename}.png',img)
 
