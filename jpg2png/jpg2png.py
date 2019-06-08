8290b8663d194e97cef9db4ac09e88af0ecbf6a0
Arthurzhangsheng <33758384+Arthurzhangsheng@users.noreply.github.com>
2019-06-08 13:41:22 +0800

    批量JPG转PNG，还可以裁剪

--- jpg2png/jpg2png.py
+++ jpg2png/jpg2png.py
@@ -2,22 +2,29 @@ import numpy as np
 import cv2
 import glob
 import os
+from tqdm import tqdm
+'''
+批量把JPG图转化格式为PNG，同时可以进行裁剪工作
+'''
 
 #待处理图片文件夹
-img_folder = r"./jpg"
+img_folder = r"F:\zs\DeepFaceLabCUDA-0219-final\workspace\AV\benzhuangling\1024aligned"
 #处理后保存的文件夹
-result_folder =r"./png" 
-
+result_folder =r"F:\zs\DeepFaceLabCUDA-0219-final\workspace\AV\benzhuangling\512png" 
+#是否裁剪
+RESIZE = True
+#缩小后的尺寸
+target_size = (512,512)
+interpolation = cv2.INTER_AREA #缩小推荐用cv2.INTER_AREA
 
 if not os.path.exists(result_folder):
     os.makedirs(result_folder)
 
-for name in glob.glob(f'{img_folder}/*.jpg'):
-    print(f"正在处理{name}")
-    
+for name in tqdm(glob.glob(f'{img_folder}/*.jpg'), ncols=10):  
     img = cv2.imread(name)
-    target_size=(512,512)
-    img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)#缩小推荐用cv2.INTER_AREA
+    if RESIZE:
+        img = cv2.resize(img, target_size, interpolation= interpolation)
     basename = os.path.basename(name).split(".")[0]#获取带后缀的基本文件名后，分割字符串，得到无后缀名字
     
     cv2.imwrite(f'{result_folder}/{basename}.png',img)
+
